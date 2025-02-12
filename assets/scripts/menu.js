1e1d0a2a457ba76bb79631faf7a740acab5794b2
Lucas Oelschlager <lucasoelschlager@gmail.com>
2025-02-12 14:24:06 -0300

    se agregaron secciones, se estilizó

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -2,6 +2,7 @@ const openMenuBtn = document.querySelector(".navbar_menuHamburguesa");
 const closeMenuBtn = document.querySelector(".menuHamburguesa_cerrar");
 const menuHamburguesa = document.querySelector(".menuHamburguesa");
 const navBar = document.querySelector(".navbar");
+const navbarTitle = document.querySelector(".navbar-title");
 openMenuBtn.addEventListener("click", () => {
   menuHamburguesa.style.display = "block";
 });
@@ -12,7 +13,8 @@ closeMenuBtn.addEventListener("click", () => {
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 689) {
-    navBar.style.backgroundColor = "rgb(24, 24, 107)";
+    navBar.style.backgroundColor = "#173b6a";
+    navbarTitle.style.backgroundColor = "#16457b";
   } else if (window.scrollY < 689) {
     navBar.style.backgroundColor = "transparent";
     navBar.style.transition = "all 0.5s";
